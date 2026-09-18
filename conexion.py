e62551402879b530767f9eba3c8e19081fa3d8ed
SebastianRonce <jronceria@ucundinamarca.edu.co>
2026-09-18 12:52:26 -0500

    1.2.1

--- conexion.py
+++ conexion.py
@@ -3,11 +3,9 @@ import pyodbc
 def get_connection():
     conn = pyodbc.connect(
         "DRIVER={ODBC Driver 17 for SQL Server};"
-        "SERVER=DESKTOP-J1HDIU5\\SQLEXPRESS;"    # ej: localhost o DESKTOP-XXX\SQLEXPRESS
+        "SERVER=DESKTOP-J1HDIU5\\SQLEXPRESS;"    
         "DATABASE=PlantCareHome;"
         "UID=sa;"
         "PWD=T1o2b3i4;"
-        # Si usas autenticación de Windows en vez de usuario/contraseña:
-        # "Trusted_Connection=yes;"
     )
     return conn
